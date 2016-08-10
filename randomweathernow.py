f4a7f8249f316df62c9955884f5324cd11db51e3
Randall Vowles <randall.vowles@utah.edu>
2016-08-10 07:43:09 -0600

    fixing bugs

--- randomweathernow.py
+++ randomweathernow.py
@@ -11,15 +11,11 @@ import simplejson
 #import bitly_api
 config = configparser.ConfigParser()
 config.read(r'/uufs/chpc.utah.edu/common/home/u0540701/randomweathernow/rwnconfig.txt')
-sectionheader = config.sections()
-section = []
-for k in range(len(sectionheader)):
-    section = sectionheader[k]
-CONSUMER_KEY = config.get(section, 'CONSUMER_KEY')
-CONSUMER_SECRET = config.get(section, 'CONSUMER_SECRET')
-ACCESS_TOKEN = config.get(section, 'ACCESS_TOKEN')
-ACCESS_TOKEN_SECRET = config.get(section, 'ACCESS_TOKEN_SECRET')
-token = config.get(section, 'token')
+CONSUMER_KEY = config.get('rwn', 'CONSUMER_KEY')
+CONSUMER_SECRET = config.get('rwn', 'CONSUMER_SECRET')
+ACCESS_TOKEN = config.get('rwn', 'ACCESS_TOKEN')
+ACCESS_TOKEN_SECRET = config.get('rwn', 'ACCESS_TOKEN_SECRET')
+token = config.get('rwn', 'token')
 #API_USER = config.get(sectionheader[0], 'API_USER')
 #API_KEY = config.get(sectionheader[0], 'API_KEY')
 #b = bitly_api.BitLy(API_USER, API_KEY)
